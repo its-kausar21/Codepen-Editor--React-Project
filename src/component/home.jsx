f2e61a1516244c29c01c29cc42313413315f3d54
Kausar <kausarnadaf21@gmail.com>
2024-09-14 17:02:05 +0530

    first commit

---
+++ src/component/home.jsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Code from  './Code';
+import Result from './Result'
+const home = () =>
+{
+    return(
+        <>
+        <Header />
+        <Code />
+        <Result />
+        </>
+    );
+}
+export default home;
